df7350a3f2cb3f6a2b894ee46d15caa017dcbed4
Mads Riis-Weitling <mrw@magenta.dk>
2025-12-15 10:32:28 +0100

    feat: [#67861] Prototype lobby and synchronized test UI
    The prototype uses a hardcoded list of multiple-choice test questions.
    
    The prototype UI is divided into two parts:
    * the "lobby", where teachers can wait for a student to log on
    * the "test" part itself, where teachers can monitor the student's test
    progress and mark questions as correct, wrong or skipped.

--- dysleksi/dysleksi/views.py
+++ dysleksi/dysleksi/views.py
@@ -1,10 +1,36 @@
 # SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
 #
 # SPDX-License-Identifier: MPL-2.0
+from typing import Any
 
+from django.utils.functional import cached_property
 from django.views.generic import TemplateView
 from login.view_mixins import LoginRequiredMixin
+from dysleksi.models import User, UserType
 
 
-class RootView(LoginRequiredMixin, TemplateView):
-    template_name = "dysleksi/base.html"
+class UserTypeMixin:
+    @cached_property
+    def user_type(self) -> UserType:
+        user = User.objects.get(pk=self.request.user.pk)
+        return user.user_type
+
+
+class RootView(LoginRequiredMixin, UserTypeMixin, TemplateView):
+    def get_template_names(self) -> list[str]:
+        return [f"dysleksi/lobby/{self.user_type.lower()}.html"]
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context_data = super().get_context_data(**kwargs)
+        # Mock student data
+        mock_student = {"id": "1234", "name": "Elev Elevsen"}
+        if self.user_type is UserType.Teacher:
+            context_data["students"] = [mock_student]
+        elif self.user_type is UserType.Student:
+            context_data["student"] = mock_student
+        return context_data
+
+
+class RoomView(LoginRequiredMixin, UserTypeMixin, TemplateView):
+    def get_template_names(self) -> list[str]:
+        return [f"dysleksi/test/{self.user_type.lower()}.html"]
