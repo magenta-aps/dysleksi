2863f22e26ac02b16bfd9eb9e0fae1847aa25d80
Nick Janssen <nj@magenta-aps.dk>
2026-01-13 10:57:55 +0100

    test: [#68233] Create dummy test for "ordlæsning 2"

---
+++ dysleksi/dysleksi/tests/test_dummy_data.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from django.core.management import call_command
+from django.test import TestCase
+
+from dysleksi.models import Test, TestPart
+
+
+class DummyDataTest(TestCase):
+    def test_dummy_data_creation(self):
+        call_command("create_dummy_tests")
+
+        word_reading_2_tests = TestPart.objects.filter(name="Wordreading 2")
+        tests = Test.objects.all()
+
+        # We expect 6 tests; each grade needs to do a test twice a year
+        self.assertEqual(tests.count(), 6)
+
+        # We expect 4 wordreading 2 tests; 1. grade does not need to do this test
+        self.assertEqual(word_reading_2_tests.count(), 4)
