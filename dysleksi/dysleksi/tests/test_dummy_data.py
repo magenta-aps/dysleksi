352f7f83b3926c41075d24b750b719d1572889ab
Nick Janssen <nj@magenta-aps.dk>
2026-04-24 10:32:37 +0200

    test: [#69781] Move heavy commands to setUpTestData to speed up tests

--- dysleksi/dysleksi/tests/test_dummy_data.py
+++ dysleksi/dysleksi/tests/test_dummy_data.py
@@ -6,20 +6,21 @@ from unittest.mock import patch
 from django.core.management import call_command
 from django.test import TestCase
 
-from dysleksi.models import (
-    PartResponse,
-    QuestionResponse,
-    Student,
-    Test,
-    TestPart,
-    TestType,
-)
+from dysleksi.management.commands.create_tests import answer_test
+from dysleksi.models import Class, QuestionResponse, Student, Test, TestPart, TestType
 
 
 class DummyDataTest(TestCase):
-    def test_dummy_data_creation(self):
-        call_command("create_tests", dummy=True)
 
+    @classmethod
+    def setUpTestData(cls):
+        """Runs once for the entire class."""
+        call_command("create_groups")
+        call_command("create_dummy_classes_and_users")
+        call_command("create_tests")
+        call_command("create_tests", dummy=True, answer=True)
+
+    def test_dummy_data_creation(self):
         # We expect exactly one "word reading 2" test part, which is reused/referenced
         # across the four available group tests ("Midt 2. klasse", "Slut 2. klasse",
         # "Midt 3. klasse", "Slut 3. klasse".) In actual test data, we would expect the
@@ -30,8 +31,6 @@ class DummyDataTest(TestCase):
         self.assertEqual(word_reading_2_test_parts.count(), 1)
 
     def test_real_dummy_data_creation(self):
-        call_command("create_tests")
-
         word_reading_2_test = TestPart.objects.filter(name="Ordlæsning 2A").first()
         wordspelling_test = TestPart.objects.filter(name="Ordstavning").first()
 
@@ -39,9 +38,6 @@ class DummyDataTest(TestCase):
         self.assertEqual(wordspelling_test.questions.count(), 25)
 
     def test_dummy_user_creation(self):
-        call_command("create_groups")
-        call_command("create_dummy_classes_and_users")
-
         self.assertTrue(
             Student.objects.filter(first_name="Elev", last_name="Elevsen").exists()
         )
@@ -53,9 +49,6 @@ class DummyDataTest(TestCase):
         self.assertEqual(amount_of_students, Student.objects.all().count())
 
     def test_dummy_test_answers_creation(self):
-        call_command("create_groups")
-        call_command("create_dummy_classes_and_users")
-        call_command("create_tests", "--answer")
         word_reading_2_test: TestPart = TestPart.objects.filter(
             name="Ordlæsning 2A"
         ).first()
@@ -79,8 +72,6 @@ class DummyDataTest(TestCase):
         )
 
     def test_dummy_test_answers_creation_no_classes_or_students(self):
-        call_command("create_groups")
-        call_command("create_tests", "--answer")
         word_reading_2_test: TestPart = TestPart.objects.filter(
             name="Ordlæsning 2A"
         ).first()
@@ -94,15 +85,11 @@ class DummyDataTest(TestCase):
         )
 
     def test_answer_test(self):
-        call_command("create_groups")
-        call_command("create_dummy_classes_and_users")
-        call_command("create_tests")
 
         pk = Test.objects.filter(test_type=TestType.GROUP).first().pk
         with patch("builtins.print") as mock_print:
             call_command("answer_test", pk)
             mock_print.assert_called_with("Must specify --class for a group test")
-            self.assertFalse(PartResponse.objects.exists())
 
         pk = Test.objects.filter(test_type=TestType.INDIVIDUAL).first().pk
         with patch("builtins.print") as mock_print:
@@ -110,4 +97,19 @@ class DummyDataTest(TestCase):
             mock_print.assert_called_with(
                 "Must specify --student for an individual test"
             )
-            self.assertFalse(PartResponse.objects.exists())
+
+    @patch("dysleksi.management.commands.create_tests.call_command")
+    def test_answer_test_if_no_student(self, mock_call_command):
+        Student.objects.all().delete()
+        test = Test.objects.filter(test_type=TestType.INDIVIDUAL).first()
+
+        answer_test(test)
+        mock_call_command.assert_not_called()
+
+    @patch("dysleksi.management.commands.create_tests.call_command")
+    def test_answer_test_if_no_class(self, mock_call_command):
+        Class.objects.all().delete()
+        test = Test.objects.filter(test_type=TestType.GROUP).first()
+
+        answer_test(test)
+        mock_call_command.assert_not_called()
