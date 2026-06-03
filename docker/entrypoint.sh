394c971a234b52971a8a635c40f90f3d83b90664
Nick Janssen <nj@magenta-aps.dk>
2026-06-03 09:48:41 +0200

    chore: [#70366] Faster startup for dev-environment
    We now run the create_tests commands in the background. So we can log in
    even before all tests have been created

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -31,8 +31,8 @@ python manage.py create_groups
 
 if [ "${CREATE_DUMMY_DATA,,}" = true ]; then
   python manage.py create_dummy_classes_and_users
-  python manage.py create_tests --dummy --answer
-  python manage.py create_tests --answer
+  python manage.py create_tests --dummy --answer &
+  python manage.py create_tests --answer &
 fi
 
 echo 'creating cache table'
