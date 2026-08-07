f84dcff50fb58d1f1d6683e2e704c32899b6bd68
Nick Janssen <nj@magenta-aps.dk>
2026-08-07 11:04:20 +0200

    fix: [#71136] show student's answers for individual tests in resultView

--- dysleksi/dysleksi/tests/test_views.py
+++ dysleksi/dysleksi/tests/test_views.py
@@ -1561,7 +1561,7 @@ class TestPartResponseView(ResponseTest):
             table,
             [
                 [["Opg."], ["Billede"], ["Rigtigt svar"], ["Elevens svar"], ["Tid"]],
-                [["1"], [], [], [], ["3 sek."]],
+                [["1"], [], [], ["TestOrd"], ["3 sek."]],
                 [["2"], [], ["TestOrd"], [], ["4 sek."]],
                 [["3"], [], [], [], ["5 sek."]],
                 [["4"], [], [], [], ["6 sek."]],
