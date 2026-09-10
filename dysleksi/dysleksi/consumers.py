9f8babd4a05720907d0aa341aaa39f59d6b64e34
Nick Janssen <nj@magenta-aps.dk>
2026-09-10 10:44:09 +0200

    fix: [#68146] Make consumers asynchronous

--- dysleksi/dysleksi/consumers.py
+++ dysleksi/dysleksi/consumers.py
@@ -1,45 +1,52 @@
 import logging
 from datetime import datetime
 
-from asgiref.sync import async_to_sync
+from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 from channels.exceptions import DenyConnection
-from channels.generic.websocket import JsonWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from django.core.cache import caches
 from django.db.models import Q
+from django_redis.cache import RedisCache
 
 from dysleksi.models import HandledEvent, Message, Student, TestAssignment
 
 logger = logging.getLogger(__name__)
-cache = caches["chat"]  # As defined in settings/cache.py
+cache: RedisCache = caches["chat"]  # As defined in settings/cache.py
 
 
-class ChatConsumer(JsonWebsocketConsumer):
-    def connect(self):
+def db_sync_to_async(func):
+    return database_sync_to_async(func, thread_sensitive=False)
+
+
+# Rendering a user queries its groups, which must not happen in the event loop
+describe = sync_to_async(str, thread_sensitive=False)
+
+
+class ChatConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
         # Ensure user is authenticated
         # cf https://www.w3tutorials.net/blog/require-login-in-a-django-channels-socket/
         self.user = self.scope["user"]
         # Reject connection if user is not authenticated
         if self.user is None or not self.user.is_authenticated:
             raise DenyConnection("User not authenticated")
+        self.user_name = await describe(self.user)
 
         # Join room group
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
-        self.accept()
-        logger.info("'%s' joined room '%s'", self.scope["user"], self.room_name)
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
+        logger.info("'%s' joined room '%s'", self.user_name, self.room_name)
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
-        logger.info("'%s' left room '%s'", self.scope["user"], self.room_name)
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        logger.info("'%s' left room '%s'", self.user_name, self.room_name)
 
     # Receive message from WebSocket
-    def receive_json(self, content: dict, **kwargs):
+    async def receive_json(self, content: dict, **kwargs):
         # required by `django-channels`, must match our method name in this class
         # (dots are replaced with underscores,
         # then the class is checked for a method with that name)
@@ -49,47 +56,49 @@ class ChatConsumer(JsonWebsocketConsumer):
             log_content["recordingBase64"] = "<redacted>"  # pragma: no cover
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(self.room_group_name, content)
-        logger.info("'%s' sent '%s'", self.user, log_content)
+        await self.channel_layer.group_send(self.room_group_name, content)
+        logger.info("'%s' sent '%s'", self.user_name, log_content)
 
         # Store message in cache
-        cache.set(
+        await cache.aset(
             f"{self.room_group_name}_{datetime.now().timestamp()}",
             content,
             timeout=300,
         )
 
         # Store message in database
-        event = content["event"]
+        if content["event"] in HandledEvent:
+            await self.store_message(content)
 
-        if event in HandledEvent:
-            # Messages send by a student contain a "student" key.
-            # Messages sent by teachers do not
-            student = content.get("student")
-            message, created = Message.objects.get_or_create(
-                uuid=content["uuid"],
-                defaults={
-                    "event": content["event"],
-                    "data": content,
-                    "user": (
-                        Student.objects.get(id=student["id"])
-                        if student is not None
-                        else self.user
-                    ),
-                },
-            )
-            if created:
-                # Only handle messages that were not already stored
-                message.handle()
+    @db_sync_to_async
+    def store_message(self, content: dict):
+        # Messages send by a student contain a "student" key.
+        # Messages sent by teachers do not
+        student = content.get("student")
+        message, created = Message.objects.get_or_create(
+            uuid=content["uuid"],
+            defaults={
+                "event": content["event"],
+                "data": content,
+                "user": (
+                    Student.objects.get(id=student["id"])
+                    if student is not None
+                    else self.user
+                ),
+            },
+        )
+        if created:
+            # Only handle messages that were not already stored
+            message.handle()
 
     # Receive message from room group
     # method name must match the type attribute in the received json
-    def chat_message(self, message: dict):
+    async def chat_message(self, message: dict):
         # Send message to WebSocket
-        self.send_json({k: v for k, v in message.items() if k != "type"})
+        await self.send_json({k: v for k, v in message.items() if k != "type"})
 
 
-class RelayConsumer(JsonWebsocketConsumer):
+class RelayConsumer(AsyncJsonWebsocketConsumer):
     """
     Consumer which allows teacher and student to talk to each other in case the webRTC
     connection fails
@@ -97,31 +106,28 @@ class RelayConsumer(JsonWebsocketConsumer):
     Does not store anything to the database; That is what the ChatConsumer is for.
     """
 
-    def connect(self):
+    async def connect(self):
         self.user = self.scope["user"]
         if self.user is None or not self.user.is_authenticated:
             raise DenyConnection("User not authenticated")
+        self.user_name = await describe(self.user)
 
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"relay_{self.room_name}"
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
-        self.accept()
-        logger.info("'%s' joined relay '%s'", self.user, self.room_name)
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
+        logger.info("'%s' joined relay '%s'", self.user_name, self.room_name)
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
-        logger.info("'%s' left relay '%s'", self.user, self.room_name)
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        logger.info("'%s' left relay '%s'", self.user_name, self.room_name)
 
-    def receive_json(self, content: dict, **kwargs):
+    async def receive_json(self, content: dict, **kwargs):
         invite_events = ("session.start", "session.in_progress")
         if self.room_name == "lobby" and content.get("event") in invite_events:
 
             # Only consider students for which this is their latest assignment.
-            content["studentIds"] = self.students_to_invite(content)
+            content["studentIds"] = await self.students_to_invite(content)
 
             if not content["studentIds"]:
                 # The lobby is for inviting students to tests.
@@ -129,8 +135,9 @@ class RelayConsumer(JsonWebsocketConsumer):
                 return
 
         content["type"] = "relay.message"
-        async_to_sync(self.channel_layer.group_send)(self.room_group_name, content)
+        await self.channel_layer.group_send(self.room_group_name, content)
 
+    @db_sync_to_async
     def students_to_invite(self, content: dict) -> list[int]:
         student_ids = content["studentIds"]
         assignment_id = content["assignmentId"]
@@ -157,5 +164,5 @@ class RelayConsumer(JsonWebsocketConsumer):
             )
         return [id for id in student_ids if id not in students_with_newer_assignments]
 
-    def relay_message(self, message: dict):
-        self.send_json({k: v for k, v in message.items() if k != "type"})
+    async def relay_message(self, message: dict):
+        await self.send_json({k: v for k, v in message.items() if k != "type"})
